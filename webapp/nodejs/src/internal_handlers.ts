8c7404e76a36e522a1df9004d3f1831f420a8014
deflis <deflis@gmail.com>
2024-12-08 12:32:22 +0900

    椅子の位置情報を取得し、ライドとの紐付け処理を更新

--- webapp/nodejs/src/internal_handlers.ts
+++ webapp/nodejs/src/internal_handlers.ts
@@ -1,7 +1,7 @@
 import type { Context } from "hono";
 import type { Environment } from "./types/hono.js";
 import type { RowDataPacket } from "mysql2";
-import type { Chair, Ride } from "./types/models.js";
+import type { Chair, ChairLocation, Ride } from "./types/models.js";
 
 // このAPIをインスタンス内から一定間隔で叩かせることで、椅子とライドをマッチングさせる
 export const internalGetMatching = async (ctx: Context<Environment>) => {
@@ -25,6 +25,22 @@ WHERE is_active = TRUE AND (rs.status = 'COMPLETED' OR rs.status IS NULL)`
     return ctx.body(null, 204);
   }
 
+  // 空いている椅子の位置情報を取得
+  const chairIds = chairs.map((chair) => chair.id);
+  const [chairLocations] = await ctx.var.dbConn.query<
+    Array<ChairLocation & RowDataPacket>
+  >(
+    `SELECT cl1.chair_id, cl1.latitude, cl1.longitude
+    FROM chair_locations cl1
+    INNER JOIN (
+      SELECT chair_id, MAX(created_at) AS latest_created_at
+      FROM chair_locations
+      WHERE chair_id IN (${chairIds.map(() => "?").join(",")})
+      GROUP BY chair_id
+    ) cl2 ON cl1.chair_id = cl2.chair_id AND cl1.created_at = cl2.latest_created_at`,
+    chairIds
+  );
+
   // ライドを取得
   const [rides] = await ctx.var.dbConn.query<Array<Ride & RowDataPacket>>(
     "SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at"
@@ -36,35 +52,39 @@ WHERE is_active = TRUE AND (rs.status = 'COMPLETED' OR rs.status IS NULL)`
     // 最も近い椅子を探す
     // 0,0 と 300, 300付近にクラスターがあるので、マンハッタン距離200で足切りする
     let minDistance = Infinity;
-    let nearestChair: (Chair & RowDataPacket) | null = null;
+    let nearestChair: (ChairLocation & RowDataPacket) | null = null;
 
     console.log(`Remaining chairs: ${chairs.length}`);
-    for (const chair of chairs) {
+    for (const chair of chairLocations) {
       const distance =
         Math.abs(chair.latitude - ride.pickup_latitude) +
         Math.abs(chair.longitude - ride.pickup_longitude);
-      console.log(`Chair ${chair.id} distance: ${distance}`);
+      console.log(`Chair ${chair.chair_id} distance: ${distance}`);
       if (distance < minDistance) {
         minDistance = distance;
         nearestChair = chair;
       }
     }
-    console.log(`Nearest chair: ${nearestChair?.id}, distance: ${minDistance}`);
+    console.log(
+      `Nearest chair: ${nearestChair?.chair_id}, distance: ${minDistance}`
+    );
 
     // ライドに椅子を紐付ける
     if (nearestChair) {
-      console.log(`Matched ride ${ride.id} with chair ${nearestChair.id}`);
+      console.log(
+        `Matched ride ${ride.id} with chair ${nearestChair.chair_id}`
+      );
       await ctx.var.dbConn.query("UPDATE rides SET chair_id = ? WHERE id = ?", [
-        nearestChair.id,
+        nearestChair.chair_id,
         ride.id,
       ]);
 
       // 紐付けた椅子を消す
-      chairs.splice(chairs.indexOf(nearestChair), 1);
-      console.log(`Remaining chairs: ${chairs.length}`);
+      chairLocations.splice(chairLocations.indexOf(nearestChair), 1);
+      console.log(`Remaining chairs: ${chairLocations.length}`);
 
       // 椅子がなくなったら終了
-      if (chairs.length === 0) {
+      if (chairLocations.length === 0) {
         break;
       }
     }
