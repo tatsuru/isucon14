f2e9aa34b8a0bf467bf4f5d513d5603a6a0c9e5e
nakataki17 <nakataki@hatena.ne.jp>
2024-12-08 15:55:48 +0900

    internalGetMatching関数に椅子の速さを考慮した距離計算を追加し、最も近い椅子を見つけるロジックを改善

--- webapp/nodejs/src/internal_handlers.ts
+++ webapp/nodejs/src/internal_handlers.ts
@@ -7,6 +7,15 @@ import type { Chair, ChairLocation, Ride } from "./types/models.js";
 export const internalGetMatching = async (ctx: Context<Environment>) => {
   await ctx.var.dbConn.beginTransaction();
   try {
+    // 椅子の速さを取得
+    const [chair_models] = await ctx.var.dbConn.query<
+      Array<{ id: string; speed: number } & RowDataPacket>
+    >(`SELECT name, speed FROM chair_models`);
+    const chairModelSpeedMap = new Map<string, number>();
+    for (const chair_model of chair_models) {
+      chairModelSpeedMap.set(chair_model.id, chair_model.speed);
+    }
+
     // ライドを取得
     const [rides] = await ctx.var.dbConn.query<Array<Ride & RowDataPacket>>(
       "SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at ASC FOR UPDATE"
@@ -42,42 +51,43 @@ export const internalGetMatching = async (ctx: Context<Environment>) => {
     // 空いている椅子の位置情報を取得
     const chairIds2 = completedChairs.map((chair) => chair.id);
     const [chairLocations] = await ctx.var.dbConn.query<
-      Array<ChairLocation & RowDataPacket>
+      Array<ChairLocation & RowDataPacket & { model: string }>
     >(
-      `SELECT cl1.chair_id, cl1.latitude, cl1.longitude
-        FROM chair_locations cl1
-        INNER JOIN (
-          SELECT chair_id, MAX(created_at) AS latest_created_at
-          FROM chair_locations
-          WHERE chair_id IN (${chairIds2.map(() => "?").join(",")})
-          GROUP BY chair_id
-        ) cl2 ON cl1.chair_id = cl2.chair_id AND cl1.created_at = cl2.latest_created_at`,
+      `SELECT cl1.chair_id, cl1.latitude, cl1.longitude, c.model
+      FROM chair_locations cl1
+      INNER JOIN (
+        SELECT chair_id, MAX(created_at) AS latest_created_at
+        FROM chair_locations
+        WHERE chair_id IN (${chairIds2.map(() => "?").join(",")})
+        GROUP BY chair_id
+      ) cl2 ON cl1.chair_id = cl2.chair_id AND cl1.created_at = cl2.latest_created_at
+      INNER JOIN chairs c ON cl1.chair_id = c.id`,
       chairIds2
     );
     //console.log(`Chair locations: ${chairLocations.length}`);
 
     // ライドと椅子をマッチング
     for (const ride of rides) {
-      //console.log(`Matching ride ${ride.id}`);
       // 最も近い椅子を探す
-      // 0,0 と 300, 300付近にクラスターがあるので、マンハッタン距離200で足切りする
-      let minDistance = 200;
+      let minTime = Infinity;
       let nearestChair: (ChairLocation & RowDataPacket) | null = null;
 
-      //console.log(`Remaining chairs: ${chairs.length}`);
       for (const chair of chairLocations) {
         const distance =
           Math.abs(chair.latitude - ride.pickup_latitude) +
           Math.abs(chair.longitude - ride.pickup_longitude);
+        if (distance > 200) {
+          // 0,0 と 300, 300付近にクラスター(都市)があるので、都市をまたぐような配車はしない
+          continue;
+        }
+        const time = distance / chairModelSpeedMap.get(chair.model)!;
         //console.log(`Chair ${chair.chair_id} distance: ${distance}`);
-        if (distance < minDistance) {
-          minDistance = distance;
+
+        if (time < minTime) {
+          minTime = time;
           nearestChair = chair;
         }
       }
-      //console.log(
-      //  `Nearest chair: ${nearestChair?.chair_id}, distance: ${minDistance}`
-      //);
 
       // ライドに椅子を紐付ける
       if (nearestChair) {
